ec7be71bd87d45fd15b8dcd8806b416c9cdc237b
Aniruddha Hardikar <aniruddha.har@gmail.com>
2020-04-26 13:29:29 +0530

    Main Project Commit
    Main project commit with assets and sample

---
+++ Assets/Scripts/playerpos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class playerpos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Shader.SetGlobalVector("_PlayerPos", transform.position);
+        
+    }
+}
